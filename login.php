cb921ff8b969ec21e137455cad00d3e9d107755e
Devendra-IT-Tech <devendrabalaji95@gmail.com>
2026-09-01 20:01:43 +0530

    [2026-09-01] Create the login and resignation page with the ui and modified the page

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="Asset/images/logo.png" >
+    <title>Welcome Back!</title>
+   <link rel="stylesheet" href="Asset/style.css">
+</head>
+<body id="body-login">
+    <div id="chumber-logo">
+        <img src="Asset/images/logo.png">
+    </div>
+<section id="login-left">
+    <img src="Asset/images/interior.jpg">
+</section>
+
+    <section id="login-right">
+    <div id="header">
+        <br>
+            <h1>Welcome back</h1>
+            <br>
+    </div>
+    <div id="login-form">
+        <form action="" method="POST">
+            <label id="email">Email:</label><br>
+            <input placeholder="Email" type="email" id="email-box" name="email"required><br>
+            <label id="password">Password:</label><br>
+            <input placeholder="Password" type="password" id="Password-box" names="password" required><br>
+            <button>LOGIN</button><br>
+            <a id="forget" href=""> Forget Password</a><br>
+            <h6>If u don't have account?</h6>
+            <a href="regsitration.php">SignUp</a>
+        </form>
+    </div>
+    </section>
+</body>
+</html>
